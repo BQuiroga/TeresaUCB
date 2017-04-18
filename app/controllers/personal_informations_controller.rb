11b37e2303e32bff0ef3dfbcab00f31dec08967b
BQuiroga <zatinador900@gmail.com>
2017-04-18 09:16:44 -0400

    path for edit personal information fixed

--- app/controllers/personal_informations_controller.rb
+++ app/controllers/personal_informations_controller.rb
@@ -8,7 +8,7 @@ class PersonalInformationsController < ApplicationController
 	def update
 		@personal=PersonalInformation.find(personal_info_params_for_edit[:id])
 		@personal.update(personal_info_params_for_edit)
-		redirect_to '/users/curriculum/edit'
+		redirect_to '/users/edit'
 	end
 	def personal_info_params
   		params.require(:personal_information).permit(:address,:user_id,:phone,:cellphone,:birthdate)
