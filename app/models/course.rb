7b423eb81d68d3385599a5d205fa67e3ea23f0de
Vinze Gonzales <vinzegonzales@Vinzes-iMac.local>
2017-04-25 02:15:32 -0400

    Edit resume view now acept 4 types of courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,13 +1,20 @@
 class Course < ActiveRecord::Base
 	belongs_to :resume
 	def orador
-		if given
-			"Orador"
-		else
+		if given == "Participante"
 			"Participante"
 		end
+		if given == "Aprobacion"
+			"Aprobacion"
+		end
+		if given == "Expositor"
+			"Expositor"
+		end
+		if given == "Organizador"
+			"Organizador"
+		end
 	end
-	def participations
+	def self.participations
 		["Participante","Aprobacion","Expositor","Organizador"]
 	end
 end
