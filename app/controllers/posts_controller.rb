ec9419af3413087a2d25614235338a47507993d8
BQuiroga <zatinador900@gmail.com>
2017-05-04 03:15:04 -0400

    job offer makes post!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,14 @@ class PostsController < ApplicationController
   end
   def offer
   end
+  def oferta
+    new_post=Post.new
+    new_body=new_post.job_offer(search_params[:titulo],search_params[:post_grado],search_params[:horas],search_params[:ciudad],search_params[:idiomas],search_params[:cargo],search_params[:experiencia],search_params[:habilidades])
+    new_post.body=new_body
+    new_post.user_id=search_params[:user_id]
+    new_post.save
+    redirect_to '/oferta/nueva'
+  end
   def create
     Post.create(post_params)
     redirect_to root_path
@@ -18,5 +26,8 @@ class PostsController < ApplicationController
   end
   def picture_params
     params.require(:post).permit(:image)
+  end
+  def search_params
+    params.require(:post).permit(:titulo,:ciudad,:post_grado,:cargo,:experiencia,:horas,:idiomas,:habilidades,:user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,34 @@ class Post < ActiveRecord::Base
   default_scope -> {order(created_at: :desc)}
   has_many :pictures
   belongs_to :user
-  def job_offer
-    ""
+
+  def job_offer(titulo,postgrado,horas,ciudad,idiomas,cargo,experiencia,habilidades)
+    resp="Estamos buscando personal, con las siguientes caracteristicas: \n"
+    if titulo
+      resp+="  Titulo Academico: "+titulo+"\n"
+    end
+    if postgrado
+      resp+="  Post Grado: "+postgrado+"\n"
+    end
+    if ciudad
+      resp+="  De la ciudad: "+ciudad+"\n"
+    end
+    if idiomas
+      resp+="  Con dominio en los siguientes idiomas: "+idiomas+"\n"
+    end
+    if cargo
+      resp+="  Que haya ocupado el cargo de: "+cargo+"\n"
+    end
+    if habilidades
+      resp+="  Con las siguientes habilidades: "+habilidades+"\n"
+    end
+    if experiencia
+      resp+="  Con experiencia en areas de: "+experiencia+"\n"
+    end
+    if horas
+      resp+="  Que haya cumplido con por lo menos: "+horas + " horas de trabajo \n"
+    end
+    resp+= "  Si tu curriculum cumple con los anteriores requisitos, comunicate con nosotros"
+    resp
   end
 end
