20d38dbe2c75c886450e995d0b6577654640d26b
BQuiroga <zatinador900@gmail.com>
2017-05-23 10:51:15 -0400

    publishing offer DONEE!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,12 @@ class UsersController < ApplicationController
 			@posts2= Post.where(user_id: follow.followed)
 			@posts=@posts+@posts2
 		end
+		@searched= Searched.where(found: current_user.id)
+		@searched.each do |searching_company|
+			@posts2= Post.where(user_id: searching_company.searched_by,requiring:true)
+			@posts =@posts +@posts2
+		end
+		@posts=@posts.uniq
+
 	end
 end
