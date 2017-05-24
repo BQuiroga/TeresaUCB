f2e9691a16418da9ad297a7688ab9a33e4f52b0f
BQuiroga <zatinador900@gmail.com>
2017-05-24 15:11:58 -0400

    first try publication email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: 'notifications@bolsadetrabajo.com'
- 
+
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
     mail(to: @user.email, subject: '¡Bienvenido a la Bolsa de Trabajo!')
+  end
+  def notify_email(user)
+    @user =  user
+    @url = 'http://example.com/login'
+    mail(to: @user.email, subject: "¡Deberias ver esto!")
   end
 end
