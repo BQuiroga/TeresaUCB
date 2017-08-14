c78c3a354c7496444f306e1556784a3f62943818
BQuiroga <zatinador900@gmail.com>
2017-08-14 15:21:36 -0400

    some modif at profile css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 	def profile
+		@grupos=Group.all
 		@posts= current_user.posts
-		@posts=@posts+current_user.follows_posts+current_user.searched_people_posts+my_friends_posts
+		@posts=@posts+current_user.follows_posts+current_user.searched_people_posts+current_user.my_friends_posts
 		@posts=@posts.uniq
 	end
 	def search
