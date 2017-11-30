5f602c833d8687038315a77064150c3fe343b542
BQuiroga <zatinador900@gmail.com>
2017-11-30 11:46:01 -0400

    some modif at resume edit

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -61,7 +61,7 @@ class Experience < ActiveRecord::Base
 			user=resume.user
 		end
       def self.salary_range
-         ["0-3000","3001-6000","6001-9000","9001-14000","14000-20000","20000+"]
+         ["0-3000","3001-6000","6001-9000","9001-14000","14000-20000","20000+","No quiero decirlo"]
       end
 			def time_in_job
 				res=Date.new
