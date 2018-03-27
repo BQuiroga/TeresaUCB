ef818bcd74d043f4aad7d1fa9ece2e7e2b594437
BQuiorga <zatinador900@gmail.com>
2018-03-27 11:23:41 -0400

    styles ALL in users.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require Chart.bundle
 //= require chartkick
 //= require_tree .
 //= require bootstrap
+//= require turbolinks
